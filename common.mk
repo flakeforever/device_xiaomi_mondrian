61de2689b1ede6b937e07dbd26c3a426662803eb
LuK1337 <priv.luk@gmail.com>
2021-01-06 19:30:19 +0100

    sm8450-common: Build vndservicemanager
    * Needed for FCM >= R.
    
    Change-Id: Ibc27a98f5774a51caaab077c1d8e0a2a79bdffe5

--- common.mk
+++ common.mk
@@ -378,6 +378,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
+# Vendor service manager
+PRODUCT_PACKAGES += \
+    vndservicemanager
+
 # Vendor init
 PRODUCT_PACKAGES += \
     charger_fw_fstab.qti \
