bcf568b2c270a56bfc47310a6f6eefa847ef7cd3
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-05 15:18:35 +0300

    sm8450-common: vibrator: add support for aw8697_haptic input device
    Change-Id: I58f5d0305ac688ba0b4d3ff5acfe04e5503b69aa

--- vibrator/aidl/Vibrator.cpp
+++ vibrator/aidl/Vibrator.cpp
@@ -106,7 +106,8 @@ InputFFDevice::InputFFDevice()
             continue;
         }
 
-        if (strcmp(name, "qcom-hv-haptics") && strcmp(name, "qti-haptics")) {
+        if (strcmp(name, "qcom-hv-haptics") && strcmp(name, "qti-haptics")
+            && strcmp(name, "aw8697_haptic")) {
             ALOGD("not a qcom/qti haptics device\n");
             close(fd);
             continue;
