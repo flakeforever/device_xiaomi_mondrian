648bae497c1fa49195044de9113af02e57c4e62c
Arian <arian.kulmer@web.de>
2022-05-04 17:10:23 +0200

    Add full stock vendor
    Change-Id: I5772920433b8734bf94f3d83605e7849cb37d33f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -10,6 +10,7 @@ include vendor/xiaomi/sm8450-common/BoardConfigVendor.mk
 COMMON_PATH := device/xiaomi/sm8450-common
 
 BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # A/B
 AB_OTA_UPDATER := true
