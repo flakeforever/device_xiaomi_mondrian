fcbb330d987d213ce7c9d07451a355ae2427f376
Arian <arian.kulmer@web.de>
2022-05-27 13:03:04 +0200

    Drop init.qcom.ssr.sh
    Change-Id: Icc9b71b3d18748eabd68e33e572653a4c930300c

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -68,14 +68,6 @@ LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.ssr.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.ssr.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
 #----------------------------------------------------------------------
 # Copy additional target-specific files
 #----------------------------------------------------------------------
