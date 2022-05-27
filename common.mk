b11379be60cff44a296ee9e336dd0c327f017146
Arian <arian.kulmer@web.de>
2022-05-27 17:50:28 +0200

    Build vibrator service
    Change-Id: Ife5d6e33902f6e8017923ad3a6f9862b51524a90

--- common.mk
+++ common.mk
@@ -117,6 +117,13 @@ PRODUCT_PACKAGES += \
     ueventd-odm.rc \
     ueventd.qcom.rc
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service.xiaomi_sm8450
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
 # Recovery
 PRODUCT_PACKAGES += \
     fastbootd
