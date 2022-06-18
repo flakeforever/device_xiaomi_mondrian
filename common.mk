98199a1d35a5e0b17e8672721f078b3d1741c597
Arian <arian.kulmer@web.de>
2022-06-18 17:18:49 +0200

    sm8450-common: Add proprietary telephony system blobs
    Change-Id: I50ee6fb860a285fe8afa8118d786f02cb76d87fc

--- common.mk
+++ common.mk
@@ -92,6 +92,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# QTI
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
+    $(LOCAL_PATH)/configs/telephony_system-ext_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/telephony_system-ext-privapp-permissions-qti.xml
+
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
