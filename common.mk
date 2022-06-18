9f3a3252d7b53302857b85076a3ffcf65fc11dc4
Arian <arian.kulmer@web.de>
2022-06-18 17:19:33 +0200

    sm8450-common: Import media init scripts
    Change-Id: I18420f26b949ee9d901dbfff92a937e6413bb6ad

--- common.mk
+++ common.mk
@@ -73,6 +73,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.qcom \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
 
+# Media
+PRODUCT_PACKAGES += \
+    init.qti.media.rc \
+    init.qti.media.sh
+
 # Overlays
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon_Sys \
