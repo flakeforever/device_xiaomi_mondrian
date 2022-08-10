12761ad6e2cf1765fcc6696cc90df80f4a45b6dc
Arian <arian.kulmer@web.de>
2022-08-10 16:45:54 +0200

    sm8450-common: Build bluetooth interfaces from source
    Change-Id: I7a843ce3c190120d74504445c213219a45bdafe2

--- common.mk
+++ common.mk
@@ -111,7 +111,10 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
-    android.hardware.bluetooth.audio@2.0-impl
+    android.hardware.bluetooth.audio@2.0-impl \
+    com.qualcomm.qti.bluetooth_audio@1.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
