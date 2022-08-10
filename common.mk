8bc09fa3fe0920d09e7f83c2166a0c16c23c41a1
Arian <arian.kulmer@web.de>
2022-08-10 18:22:14 +0200

    sm8450-common: Build capabilityconfigstore from source
    Change-Id: Id71ccfe15b86054b151ef253fd00c93b4e35746e

--- common.mk
+++ common.mk
@@ -147,6 +147,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Capabilityconfigstore
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.capabilityconfigstore@1.0.vendor
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey
