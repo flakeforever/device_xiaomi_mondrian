7325c171908cae6dc8c69b8d5003fe0ff43f73dd
Arian <arian.kulmer@web.de>
2022-08-10 18:35:55 +0200

    sm8450-common: Build camera interfaces from source
    Change-Id: Iacdfcc56935abb5b7d1204c9af6ccb0230dc6f0d

--- common.mk
+++ common.mk
@@ -135,7 +135,9 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.7.vendor \
     camera.device@1.0-impl \
-    libcamera2ndk_vendor
+    libcamera2ndk_vendor \
+    vendor.qti.hardware.camera.aon@1.0.vendor \
+    vendor.qti.hardware.camera.postproc@1.0.vendor
 
 PRODUCT_PACKAGES += \
     libMegviiFacepp-0.5.2 \
