6629c9181695c1a826f039f452c195ec1e3fa810
Arian <arian.kulmer@web.de>
2022-09-15 11:38:33 +0200

    sm8450-common: udfps: Handle fingerprint enroll vendor code
    Change-Id: I89c3f855b09e2fd8f34bfa93f1cbb22d41f7f65b

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -65,8 +65,11 @@ class XiaomiSm8450UdfpsHander : public UdfpsHandler {
         if (result == FINGERPRINT_ACQUIRED_GOOD) {
             setFingerDown(false);
             setFodStatus(FOD_STATUS_OFF);
-        } else if (vendorCode == 21) {
-            // vendorCode 21 means waiting for fingerprint, so enable fod listening
+        } else if (vendorCode == 21 || vendorCode == 23) {
+            /*
+             * vendorCode = 21 waiting for fingerprint authentication
+             * vendorCode = 23 waiting for fingerprint enroll
+             */
             setFodStatus(FOD_STATUS_ON);
         }
     }
