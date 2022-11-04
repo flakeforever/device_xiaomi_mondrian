97f552bc0cde343a3c383b5eb48866dd2911d8c0
haohuang <quic_haohuang@quicinc.com>
2022-11-04 09:02:45 +0800

    mondrian: gps: Remove seccomp_policy of xtwifi-inet-client
    Change-Id: Ic7b0f42db15aa41a90688590c8953b1e234ebdee
    CRs-Fixed: 3324736

--- gps/gps_vendor_product.mk
+++ gps/gps_vendor_product.mk
@@ -7,7 +7,6 @@ PRODUCT_PACKAGES += gnss_antenna_info.conf
 PRODUCT_PACKAGES += gnss@2.0-base.policy
 PRODUCT_PACKAGES += gnss@2.0-xtra-daemon.policy
 PRODUCT_PACKAGES += gnss@2.0-xtwifi-client.policy
-PRODUCT_PACKAGES += gnss@2.0-xtwifi-inet-agent.policy
 PRODUCT_PACKAGES += libloc_pla_headers
 PRODUCT_PACKAGES += liblocation_api_headers
 PRODUCT_PACKAGES += libgps.utils_headers
