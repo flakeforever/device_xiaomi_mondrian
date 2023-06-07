62a655e08325d139c531a9e410fddb29de11c5ce
Michael Bestas <mkbestas@gmail.com>
2023-06-07 23:28:37 +0300

    mondrian: gps: Remove all Wno-error flags
    Almost all warnings/errors have been fixed.
    Ignore undefined bool conversion error on purpose.
    
    Change-Id: I00cb5f38711fa9f873da030fc6a0ebc6cde3ceeb

--- gps/Android.mk
+++ gps/Android.mk
@@ -10,19 +10,7 @@ GNSS_CFLAGS := \
     -Wunused-function \
     -Wimplicit-fallthrough \
     -Wno-unused-parameter \
-    -Wno-error=unused-parameter \
-    -Wno-error=macro-redefined \
-    -Wno-error=reorder \
-    -Wno-error=missing-braces \
-    -Wno-error=self-assign \
-    -Wno-error=enum-conversion \
-    -Wno-error=logical-op-parentheses \
-    -Wno-error=null-arithmetic \
-    -Wno-error=null-conversion \
-    -Wno-error=parentheses-equality \
-    -Wno-error=undefined-bool-conversion \
-    -Wno-error=tautological-compare \
-    -Wno-error=switch
+    -Wno-undefined-bool-conversion
 
 GNSS_HIDL_VERSION := 2.1
 GNSS_AIDL_VERSION := 1
