f26a17ed416ecfed1127e81141983731fc85ba7f
Arian <arian.kulmer@web.de>
2023-06-25 19:21:34 +0200

    sm8450-common: Import touch_report to recovery
    Change-Id: I5632786499af3bb0f27854dc547f436e5fd5166b

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -84,7 +84,9 @@ LOCAL_MODULE       := init.touch_report.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/$(LOCAL_MODULE)
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
+LOCAL_MODULE_PATH  :=  \
+    $(TARGET_OUT_VENDOR_ETC)/init/$(LOCAL_MODULE) \
+    $(TARGET_RECOVERY_ROOT_OUT)/vendor/etc/init
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
