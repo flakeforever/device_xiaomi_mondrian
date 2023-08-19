98e90f836197e576f3ff442a18a1b6548e76c040
Arian <arian.kulmer@web.de>
2023-08-19 17:14:42 +0200

    sm8450-common: Add hotword enrollment blobs
    Change-Id: I4ddb3dcf85ab45b02808cbe3d28cb8caa2858dca

--- common.mk
+++ common.mk
@@ -240,6 +240,10 @@ PRODUCT_PACKAGES += \
     libhidltransport.vendor \
     libhwbinder.vendor
 
+# Hotword Enrollement
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
+
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
