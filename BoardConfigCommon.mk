98494bf9dcf904366119b5e1c31f7c718547ce6b
Arian <arian.kulmer@web.de>
2023-08-19 16:24:05 +0200

    sm8450-common: Declare source of extracted files and set vendor spl
    Change-Id: Ibe4fb0bf60237cf2aa85fe7566f04b4f2fb5bb7b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -207,6 +207,9 @@ BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE := true
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := true
 
+# Security patch level
+VENDOR_SECURITY_PATCH := 2023-07-01
+
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
