4db7023e61f4cd257ae64f8359e654cbe7b66f6a
Jens Reidel <adrian@travitia.xyz>
2023-11-15 18:22:43 +0100

    sm8450-common: rootdir: Update init.qcom.early_boot.sh from marble V14.0.9.0.TMREUXM
    Change-Id: Ifc366eaa4b8256fba9ebc92d26968df27491eeb1
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- rootdir/etc/init.qcom.early_boot.sh
+++ rootdir/etc/init.qcom.early_boot.sh
@@ -95,6 +95,9 @@ function set_density_by_fb() {
             "mondrian")
             setprop vendor.display.lcd_density 560
             ;;
+            "marble")
+            setprop vendor.display.lcd_density 440
+            ;;
             *)
             setprop vendor.display.lcd_density 480
             ;;
@@ -136,6 +139,9 @@ function set_density_by_fb() {
             setprop vendor.display.lcd_density 440
             ;;
             "ziyi")
+            setprop vendor.display.lcd_density 440
+            ;;
+            "marble")
             setprop vendor.display.lcd_density 440
             ;;
         esac
