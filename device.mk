b03d879aaf2321677997d6059e073e2c939aa9d1
mountaser <mountaser1998@gmail.com>
2024-02-25 20:05:02 +0800

    mondrian: Import powershare from cupid
    - Ported from https://github.com/LineageOS android_device_oneplus_sm8250-common
    - Requires LineageOS/android_frameworks_base@0769646
    - Adapted for cupid
    
    Co-authored-by: Luk1337 <priv.luk@gmail.com>
    Signed-off-by: flakeforever <113103193+flakeforever@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -365,7 +365,7 @@ PRODUCT_COPY_FILES += \
 
 # PowerShare
 PRODUCT_PACKAGES += \
-    vendor.lineage.powershare@1.0-service.xiaomi
+    vendor.lineage.powershare@1.0-service.xiaomi_mondrian
 
 # QTI
 PRODUCT_COPY_FILES += \
