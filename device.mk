b6c9fb755d3ea9e67837698c84561d772c1fa27a
flakeforever <113103193+flakeforever@users.noreply.github.com>
2024-02-25 21:07:28 +0800

    mondrian: Switch android.hardware.drm@1.4-service.clearkey to vendor
    Signed-off-by: flakeforever <113103193+flakeforever@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -213,7 +213,7 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm@1.4-service.clearkey.vendor \
     libdrm.vendor
 
 # Dumpstate
