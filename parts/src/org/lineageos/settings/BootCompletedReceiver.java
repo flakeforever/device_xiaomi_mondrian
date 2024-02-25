1945929e90b4a278c1dbcef755890ef6d2f67174
LuK1337 <priv.luk@gmail.com>
2024-02-25 19:10:47 +0800

    mondrian: Import XiaomiParts from marble
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: AnierinB <anierin@evolution-x.org>
    Co-authored-by: Ardjlon <carlosardany@gmail.com>
    Co-authored-by: Arian <arian.kulmer@web.de>
    Co-authored-by: arnodorian-r <geekroy98@gmail.com>
    Co-authored-by: cristianvaz <cristianvaz81993@gmail.com>
    Co-authored-by: daniml3 <danimoral1001@gmail.com>
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Co-authored-by: ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Co-authored-by: Joey Huab <joey@evolution-x.org>
    Co-authored-by: johnmart19 <johnivan19999@gmail.com>
    Co-authored-by: lostark13 <tushar9915@gmail.com>
    Co-authored-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Co-authored-by: nullxception <enuma.alrizky@gmail.com>
    Co-authored-by: Pig <pig.priv@gmail.com>
    Co-authored-by: Pulkit077 <pulkitagarwal2k1@gmail.com>
    Co-authored-by: rdxzv <rdxzv.dev@gmail.com>
    Co-authored-by: saikiran2001 <bjsaikiran@gmail.com>
    Co-authored-by: Sebastiano Barezzi <seba@sebaubuntu.dev>
    Co-authored-by: WSTxda <wstxda@gmail.com>
    Co-authored-by: Yuan Si <do4suki@gmail.com>
    Co-authored-by: YuKongA <70465933+YuKongA@users.noreply.github.com>
    Signed-off-by: flakeforever <113103193+flakeforever@users.noreply.github.com>

---
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *               2017-2020 The LineageOS Project
+ * Copyright (C) 2023 Paranoid Android
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.settings;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.display.DisplayManager;
+import android.content.IntentFilter;
+import android.util.Log;
+import android.view.Display;
+import android.content.SharedPreferences;
+import android.os.SystemProperties;
+import androidx.preference.PreferenceManager;
+import android.os.IBinder;
+import android.view.Display.HdrCapabilities;
+
+import org.lineageos.settings.display.ColorService;
+import org.lineageos.settings.dolby.DolbyUtils;
+import org.lineageos.settings.doze.AodBrightnessService;
+import org.lineageos.settings.doze.DozeUtils;
+import org.lineageos.settings.doze.PocketService;
+import org.lineageos.settings.display.ColorService;
+import org.lineageos.settings.thermal.ThermalUtils;
+import org.lineageos.settings.refreshrate.RefreshUtils;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+
+    private static final boolean DEBUG = false;
+    private static final String TAG = "XiaomiParts-BCR";
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        if (DEBUG) Log.d(TAG, "Received boot completed intent");
+        ThermalUtils.startService(context);
+
+        Log.i(TAG, "Boot completed");
+
+        // Dolby Atmos
+        DolbyUtils.getInstance(context);
+
+        // Doze
+        DozeUtils.checkDozeService(context);
+
+        // Pocket
+        PocketService.startService(context);
+
+        // DisplayFeature
+        ColorService.startService(context);
+
+        // AOD
+        AodBrightnessService.startService(context);
+
+        // Per app refresh rate
+        RefreshUtils.startService(context);
+
+        // Override HDR types
+        final DisplayManager displayManager = context.getSystemService(DisplayManager.class);
+        displayManager.overrideHdrTypes(Display.DEFAULT_DISPLAY, new int[]{
+                HdrCapabilities.HDR_TYPE_DOLBY_VISION, HdrCapabilities.HDR_TYPE_HDR10,
+                HdrCapabilities.HDR_TYPE_HLG, HdrCapabilities.HDR_TYPE_HDR10_PLUS});
+    }
+}
---
+++ parts/src/org/lineageos/settings/dolby/DolbyTileService.java
@@ -0,0 +1,36 @@
+package org.lineageos.settings.dolby;
+
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+public class DolbyTileService extends TileService {
+
+    @Override
+    public void onStartListening() {
+        Tile tile = getQsTile();
+        DolbyUtils dolbyUtils = DolbyUtils.getInstance(getApplicationContext());
+        if (dolbyUtils.getDsOn()) {
+            tile.setState(Tile.STATE_ACTIVE);
+        } else {
+            tile.setState(Tile.STATE_INACTIVE);
+        }
+        tile.setSubtitle(dolbyUtils.getProfileName());
+        tile.updateTile();
+        super.onStartListening();
+    }
+
+    @Override
+    public void onClick() {
+        Tile tile = getQsTile();
+        DolbyUtils dolbyUtils = DolbyUtils.getInstance(getApplicationContext());
+        if (dolbyUtils.getDsOn()) {
+            dolbyUtils.setDsOn(false);
+            tile.setState(Tile.STATE_INACTIVE);
+        } else {
+            dolbyUtils.setDsOn(true);
+            tile.setState(Tile.STATE_ACTIVE);
+        }
+        tile.updateTile();
+        super.onClick();
+    }
+}
