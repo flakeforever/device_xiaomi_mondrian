6cb32c803ed7bb1b429aad2b0a59aefde867b7a1
LuK1337 <priv.luk@gmail.com>
2024-02-25 19:10:47 +0800

    mondrian: Import XiaomiParts from marble
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: AnierinB <anierin@evolution-x.org>
    Co-authored-by: Ardjlon <carlosardany@gmail.com>
    Co-authored-by: Arian <arian.kulmer@web.de>
    Co-authored-by: arnodorian-r <geekroy98@gmail.com>
    Co-authored-by: cristianvaz <cristianvaz81993@gmail.com>
    Co-authored-by: daniml3 <danimoral1001@gmail.com>
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Co-authored-by: ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Co-authored-by: Joey Huab <joey@evolution-x.org>
    Co-authored-by: johnmart19 <johnivan19999@gmail.com>
    Co-authored-by: lostark13 <tushar9915@gmail.com>
    Co-authored-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Co-authored-by: nullxception <enuma.alrizky@gmail.com>
    Co-authored-by: Pig <pig.priv@gmail.com>
    Co-authored-by: Pulkit077 <pulkitagarwal2k1@gmail.com>
    Co-authored-by: rdxzv <rdxzv.dev@gmail.com>
    Co-authored-by: saikiran2001 <bjsaikiran@gmail.com>
    Co-authored-by: Sebastiano Barezzi <seba@sebaubuntu.dev>
    Co-authored-by: WSTxda <wstxda@gmail.com>
    Co-authored-by: Yuan Si <do4suki@gmail.com>
    Co-authored-by: YuKongA <70465933+YuKongA@users.noreply.github.com>
    Signed-off-by: flakeforever <113103193+flakeforever@users.noreply.github.com>

---
+++ parts/src/org/lineageos/settings/camera/NfcCameraService.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright (C) 2023 Paranoid Android
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.settings.camera;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraManager;
+import android.nfc.NfcAdapter;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.UserHandle;
+import android.util.Log;
+
+import java.util.Set;
+
+public class NfcCameraService extends Service {
+    private static final String TAG = "NfcCameraService";
+    private static final boolean DEBUG = true;
+
+    private static final int MAX_POLLING_PAUSE_TIMEOUT = 40000;
+    private static final String FRONT_CAMERA_ID = "1";
+
+    private static final Set<String> IGNORED_PACKAGES = Set.of(
+        "org.pixelexperience.faceunlock", // face unlock
+        "com.google.android.as" // auto rotate, screen attention etc
+    );
+
+    private NfcAdapter mNfcAdapter;
+    private CameraManager mCameraManager;
+    private boolean mIsFrontCamInUse = false;
+
+    private final Handler mHandler = new Handler();
+
+    private final CameraManager.AvailabilityCallback mCameraCallback =
+            new CameraManager.AvailabilityCallback() {
+        @Override
+        public void onCameraOpened(String cameraId, String packageId) {
+            dlog("onCameraOpened id=" + cameraId + " package=" + packageId);
+            if (cameraId.equals(FRONT_CAMERA_ID) && !IGNORED_PACKAGES.contains(packageId)) {
+                mIsFrontCamInUse = true;
+                updateNfcPollingState();
+            }
+        }
+
+        @Override
+        public void onCameraClosed(String cameraId) {
+            dlog("onCameraClosed id=" + cameraId);
+            if (cameraId.equals(FRONT_CAMERA_ID) && mIsFrontCamInUse) {
+                mIsFrontCamInUse = false;
+                updateNfcPollingState();
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        dlog("onCreate");
+        mCameraManager = getSystemService(CameraManager.class);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        dlog("onStartCommand");
+        mCameraManager.registerAvailabilityCallback(mCameraCallback, mHandler);
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        dlog("onDestroy");
+        mCameraManager.unregisterAvailabilityCallback(mCameraCallback);
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    public static void startService(Context context) {
+        if (!context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_NFC)) {
+            Log.i(TAG, "No nfc on this device, won't start service!");
+            return;
+        }
+        context.startServiceAsUser(new Intent(context, NfcCameraService.class), UserHandle.CURRENT);
+    }
+
+    private NfcAdapter getNfcAdapter() {
+        if (mNfcAdapter == null) {
+            dlog("getNfcAdapter: mNfcAdapter=null");
+            try {
+                mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+            } catch (Exception e) {
+                Log.e(TAG, "getNfcAdapter failed!", e);
+            }
+        }
+        return mNfcAdapter;
+    }
+
+    private void updateNfcPollingState() {
+        final NfcAdapter adapter = getNfcAdapter();
+        if (adapter == null) {
+            Log.e(TAG, "updateNfcPollingState: NfcAdapter is null!");
+            return;
+        }
+        if (!adapter.isEnabled()) {
+            dlog("updateNfcPollingState: nfc is disabled");
+            return;
+        }
+        if (mIsFrontCamInUse) {
+            Log.i(TAG, "Front cam in use, pause polling");
+            pausePolling(adapter);
+        } else {
+            Log.i(TAG, "Front cam not in use, resume polling");
+            mHandler.removeCallbacksAndMessages(null);           
+            adapter.resumePolling();
+        }
+    }
+
+    private void pausePolling(NfcAdapter adapter) {
+        adapter.pausePolling(MAX_POLLING_PAUSE_TIMEOUT);
+        mHandler.postDelayed(() -> {
+            if (adapter.isEnabled()) {
+                Log.i(TAG, "Front cam still in use, polling pause timed out, pausing again");
+                pausePolling(adapter);
+            } else {
+                dlog("pausePolling: nfc is disabled");
+            }
+        }, MAX_POLLING_PAUSE_TIMEOUT + 100);
+    }
+
+    private static void dlog(String msg) {
+        if (DEBUG) Log.d(TAG, msg);
+    }
+}
