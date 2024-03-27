5afc0d3ece07b501343877ea6c4e4a76a05a2c5d
Peter Kalauskas <Peter Kalauskas@users.noreply.gihub.com>
2024-03-27 05:08:54 -0400

    mondrian: parts: Enable use_resource_processor for all sysui deps
    Signed-off-by: flakeforever <113103193+flakeforever@users.noreply.github.com>

--- parts/src/org/lineageos/settings/dolby/DolbyActivity.java
+++ parts/src/org/lineageos/settings/dolby/DolbyActivity.java
@@ -19,7 +19,6 @@ package org.lineageos.settings.dolby;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.widget.R;
 
 public class DolbyActivity extends CollapsingToolbarBaseActivity {
 
@@ -29,7 +28,7 @@ public class DolbyActivity extends CollapsingToolbarBaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+        getFragmentManager().beginTransaction().replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame,
                 new DolbySettingsFragment(), TAG_DOLBY).commit();
     }
 }
--- parts/src/org/lineageos/settings/speaker/ClearSpeakerActivity.java
+++ parts/src/org/lineageos/settings/speaker/ClearSpeakerActivity.java
@@ -19,7 +19,6 @@ package org.lineageos.settings.speaker;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.widget.R;
 
 public class ClearSpeakerActivity extends CollapsingToolbarBaseActivity {
 
@@ -29,7 +28,7 @@ public class ClearSpeakerActivity extends CollapsingToolbarBaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+        getFragmentManager().beginTransaction().replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame,
                 new ClearSpeakerFragment(), TAG_CLEARSPEAKER).commit();
     }
 }
