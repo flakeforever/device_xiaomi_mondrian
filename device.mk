0d9be4434fa35501d24e48e60de9f601ae908467
flakeforever <113103193+flakeforever@users.noreply.github.com>
2024-06-22 14:33:09 +0800

    mondrian: Switch health HAL to healthd-ext

--- device.mk
+++ device.mk
@@ -252,8 +252,8 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl-qti \
-    android.hardware.health@2.1-service
+    android.hardware.health-service.qti \
+    android.hardware.health-service.qti_recovery
 
 # HIDL
 PRODUCT_PACKAGES += \
